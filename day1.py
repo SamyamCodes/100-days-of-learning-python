957ceed29395e2e8f9316611223372e5c7a58d2b
Samyam Shrestha <samyamshrestha48@gmail.com>
2021-05-31 22:35:14 +0545

    This is day 2

--- day1.py
+++ day1.py
@@ -1,12 +1,14 @@
+# this program is basic
 name = input("Enter your name: ")
 length = len(name)
 surname = input("Enter your surname: ")
 length1 = len(surname)
+# this code lines are for displaying the value in reverse way
 change = name
 name = surname
 surname = change
 
-
+#here, str() lets us to convert inteher to string .
 print("Your name has " + str(length) + 'words')
 print("Your surname has " + str(length1) + 'words.')
 print("Your surname is: " + name)
