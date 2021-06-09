2240fabfc9cc1bc3f553237bc1070ace15e71d52
Samyam Shrestha <samyamshrestha48@gmail.com>
2021-06-09 23:43:31 +0545

    📦 added lists program

---
+++ Day 4/lists.py
@@ -0,0 +1,26 @@
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+print(states_of_america)
+print(states_of_america[0])
+print(states_of_america[4])
+print(states_of_america[-1]) #This prints the last item
+print(states_of_america[-2]) #This prints second last item.
+
+# [-1] Here, -1 is called index value.
+
+states_of_america[1]= "Pencilvania" # We can change the list value that is inside by this way.
+
+
+# Add a  single item at end of list >> use .append() function
+ 
+# eg:
+states_of_america.append("Angela land") # This adds Angela land at the end of the list
+
+states_of_america.extend("Johnnyland", "SamyamLand", "landof earth") #.extend() >> extends the list. Adds list to the end.
+
+
+
+
+
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears", "Tomatoes", "Celery", "Potatoes"]
+
