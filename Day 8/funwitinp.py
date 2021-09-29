e0b16add47dc9b3af597a1b5b7168734815c2745
Samyam Shrestha <samyamshrestha48@gmail.com>
2021-09-29 12:32:54 +0545

    🚀 here is function with input.

---
+++ Day 8/funwitinp.py
@@ -0,0 +1,16 @@
+# Simple function
+def greet():
+    print("How do you do?")
+    print("Helo Samyam")
+    print("What's the weather today?")
+
+greet()
+
+# Function that allows for input
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+greet_with_name("Samyam")
+    
